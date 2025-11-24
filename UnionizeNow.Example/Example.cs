d3b19f25bc0348538d97258584cea758a59c6ab2
IanWold <ilwold@hotmail.com>
2025-11-23 18:28:23 -0600

    Add comments to Example

--- UnionizeNow.Example/Example.cs
+++ UnionizeNow.Example/Example.cs
@@ -8,6 +8,9 @@ public abstract partial record Side : IUnionizeNow {
 public abstract partial record Option<T> : IUnionizeNow {
     public partial record Some(T Result);
     public partial record None;
+
+    // Need to manually add this now; may generate in future
+    public static implicit operator Option<T>(T result) => new Some(result);
 }
 
 public static class Class1 {
@@ -21,14 +24,17 @@ public static class Class1 {
         IScab => true
     };
 
+    // This triggers no warning! It's complete!
+    // Further, this demonstrates you can use both the generated interface and the union.member notation:
     public static bool IsUnionist() => ChooseSide("Eugene") switch {
         IUnionist => true,
         Side.Scab => false
     };
 
+    // The _ case demonstrates using the manually-added implicit conversion from T
     public static Option<int> Divide(int first, int second) => second switch {
         0 => new None(),
-        _ => new Option<int>.Some(first / second)
+        _ => first / second
     };
 
     public static bool CanDivide(int first, int second) => Divide(first, second) switch {
